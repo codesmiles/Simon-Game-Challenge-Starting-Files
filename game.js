fbf6fab30fde53e1418f8b015bda4a7f91829e16
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-02-10 20:23:16 +0100

    Interesting Update

--- game.js
+++ game.js
@@ -1,22 +1,31 @@
 const buttonColours = ["red", "blue", "green", "yellow"];
 
 const gamePattern = [];
+const userClickedPattern = [];
 
 function nextSequence() {
-
   var randomNumber = Math.floor(Math.random() * 4); //Random number between 0 and 3
   var randomChosenColour = buttonColours[randomNumber]; //randomChosenColour is the random color from the array of colours
   gamePattern.push(randomChosenColour); //  push the random colors generated into the gamePattern array
   $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100); // Pick random divs with random colours
+
   // Play sound
-  $(`#${randomChosenColour}`).ready(function () {
-    const audio = new Audio(`sounds/${randomChosenColour}.mp3`);
-    audio.play();
-  }).click(function () { 
-    let userChosenColour =   `clicked` ; //$(this).attr("id");
-    return userChosenColour;
-  });
- 
+  $(`#${randomChosenColour}`)
+    .ready(function () {
+      const audio = new Audio(`sounds/${randomChosenColour}.mp3`);
+      audio.play();
+    })
+    .click(function () {
+      // animate when the color is clicked
+      $(this).fadeOut(100).fadeIn(100); // Pick random divs with random colours
+      let userChosenColour = $(this).attr("id");
+      userClickedPattern.push(userChosenColour);
+    
+      // Play sound when clicked
+      const audio = new Audio(`sounds/${userChosenColour}.mp3`);
+      audio.play();
+    })
+  
 }
 
 // $("document").ready(function () {

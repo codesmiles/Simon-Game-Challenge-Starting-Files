c0668b9e77040ebded03124e20ef1fb32174edef
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-02-10 21:36:40 +0100

    New insight update

--- game.js
+++ game.js
@@ -9,23 +9,24 @@ function nextSequence() {
   gamePattern.push(randomChosenColour); //  push the random colors generated into the gamePattern array
   $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100); // Pick random divs with random colours
 
-  // Play sound
-  $(`#${randomChosenColour}`)
-    .ready(function () {
-      const audio = new Audio(`sounds/${randomChosenColour}.mp3`);
-      audio.play();
-    })
-    .click(function () {
-      // animate when the color is clicked
-      $(this).fadeOut(100).fadeIn(100); // Pick random divs with random colours
-      let userChosenColour = $(this).attr("id");
-      userClickedPattern.push(userChosenColour);
-    
-      // Play sound when clicked
-      const audio = new Audio(`sounds/${userChosenColour}.mp3`);
-      audio.play();
-    })
-  
+  playSound().click(function () {
+    // animate when the color is clicked
+    $(this).fadeOut(100).fadeIn(100); // Pick random divs with random colours
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    // Play sound when clicked
+    const audio = new Audio(`sounds/${userChosenColour}.mp3`);
+    audio.play();
+  });
+}
+
+const playSound = (name)=> {
+  // Play sound based on random number
+  $(`#${randomChosenColour}`).ready(function () {
+    const audio = new Audio(`sounds/${randomChosenColour}.mp3`);
+    audio.play();
+  });
 }
 
 // $("document").ready(function () {

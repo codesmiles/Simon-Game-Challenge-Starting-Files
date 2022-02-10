55e3950b1bc439ad0af9c93e26d4180167536fbb
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-02-10 19:47:53 +0100

    Update

--- game.js
+++ game.js
@@ -3,18 +3,20 @@ const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
 
 function nextSequence() {
+
   var randomNumber = Math.floor(Math.random() * 4); //Random number between 0 and 3
-
   var randomChosenColour = buttonColours[randomNumber]; //randomChosenColour is the random color from the array of colours
-
   gamePattern.push(randomChosenColour); //  push the random colors generated into the gamePattern array
-
   $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100); // Pick random divs with random colours
-
+  // Play sound
   $(`#${randomChosenColour}`).ready(function () {
     const audio = new Audio(`sounds/${randomChosenColour}.mp3`);
     audio.play();
+  }).click(function () { 
+    let userChosenColour =   `clicked` ; //$(this).attr("id");
+    return userChosenColour;
   });
+ 
 }
 
 // $("document").ready(function () {

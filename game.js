e4e74bedf498fb7c495744d0a4b70088c4d19e1d
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-02-12 23:36:27 +0100

    UPdateeee

--- game.js
+++ game.js
@@ -10,16 +10,13 @@ $(document).keypress(function (e) {
   $("h1").text(`Level ${level}`); // leveling up
 });
 
-// checking answers
-function checkAnswer(currentLevel) {
-  currentLevel = level;
-  // let Level = gamePattern[gamePattern.length - 1];
-}
+
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4); //0 - 3
   var randomChosenColour = buttonColours[randomNumber]; //random color from the array of buttonColours
   gamePattern.push(randomChosenColour); //  push the random colors generated into the gamePattern array
+
   $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100); // Pick random divs with random colours
   animatePress();
 
@@ -30,13 +27,16 @@ function nextSequence() {
     animatePress(randomChosenColour);
     let userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+    // console.log(userClickedPattern);
 
     // Play sound when clicked
     const audio = new Audio(`sounds/${userChosenColour}.mp3`);
     audio.play();
 
-    $(`h1`).text(`Level ${++level}`); //Incementing levels
+    checkAnswer(userChosenColour); //check answer after user clicked
   });
+
+  $(`h1`).text(`Level ${++level}`); //Incementing levels
 
   // Play sound based on random number
   function playSound(name) {
@@ -51,6 +51,18 @@ function nextSequence() {
     return setTimeout(function () {
       $(`#${currentColour}`).removeClass("pressed");
     }, 100);
+  }
+
+  // checking answers
+  function checkAnswer(currentLevel) {
+    let Level = userClickedPattern.lastIndexOf(currentLevel)
+    
+    if (gamePattern.indexOf(currentLevel) == gamePattern.length-1) {
+      console.log(`success`)
+    }
+    else {
+      console.log(`wrong`)
+    }
   }
 }
 

b14dcb29e8e9ad6b452c57ed43eec714311b3d0d
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-02-14 22:31:43 +0100

    Update

--- game.js
+++ game.js
@@ -10,6 +10,37 @@ $(document).keypress(function (e) {
   $("h1").text(`Level ${level}`); // leveling up
 });
 
+// checking answers
+function checkAnswer(currentLevel) {
+  currentLevel = currentLevel.toString();
+
+  if (currentLevel === gamePattern[gamePattern.length - 1]) { 
+    console.log(
+      "success",
+      gamePattern,
+      `Clicked pattern: ${userClickedPattern}`
+    );
+ 
+  }
+  // if (gamePattern.indexOf(currentLevel) === gamePattern.length - 1) {
+  //   console.log(
+  //     `success`,
+  //     `Clicked pattern: ${userClickedPattern}`,
+  //     `gamePattern: ${gamePattern}`
+  //   );
+  // }
+  // nextSequence();
+  // if (nextSequence() === true) {
+  //   setInterval(function () {
+
+  //   }, 1000);
+  // }
+  // else {
+  //   playSound("wrong");
+  //   // nextSequence();
+  // }
+}
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4); //0 - 3
   var randomChosenColour = buttonColours[randomNumber]; //random color from the array of buttonColours
@@ -24,44 +55,31 @@ function nextSequence() {
     $(this).fadeOut(100).fadeIn(100);
     animatePress(randomChosenColour);
     let userChosenColour = $(this).attr("id");
+    checkAnswer(userChosenColour); //check answer after user clicked
     userClickedPattern.push(userChosenColour);
-    // console.log(userClickedPattern);
 
     // Play sound when clicked
     const audio = new Audio(`sounds/${userChosenColour}.mp3`);
     audio.play();
-
-    checkAnswer(userChosenColour); //check answer after user clicked
   });
 
   $(`h1`).text(`Level ${++level}`); //Incementing levels
+}
 
-  // Play sound based on random number
-  function playSound(name) {
-    return $(`#${name}`).ready(() => {
-      const audio = new Audio(`sounds/${name}.mp3`);
-      audio.play();
-    });
-  }
+// To animate keys
+function animatePress(currentColour) {
+  $(`#${currentColour}`).addClass("pressed");
+  return setTimeout(() => {
+    $(`#${currentColour}`).removeClass("pressed");
+  }, 100);
+}
 
-  function animatePress(currentColour) {
-    $(`#${currentColour}`).addClass("pressed");
-    return setTimeout(function () {
-      $(`#${currentColour}`).removeClass("pressed");
-    }, 100);
-  }
-
-  // checking answers
-  function checkAnswer(currentLevel) {
-    currentLevel = currentLevel.toString();
-    let Level = userClickedPattern.lastIndexOf(currentLevel);
-
-    if (gamePattern.indexOf(currentLevel) === gamePattern.length - 1) {
-      console.log(`success`);
-    } else {
-      console.log(`wrong`);
-    }
-  }
+// Play sound based on random number
+function playSound(name) {
+  return $(`#${name}`).ready(() => {
+    const audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
+  });
 }
 
 // $("document").ready(function () {

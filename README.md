9dc25c8a1b172020fada04c36c3398e92580f808
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-02-15 00:14:41 +0100

    Almost final update

---
+++ README.md
@@ -0,0 +1,8 @@
+# SIMON'S GAME
+
+## About the game
+Alaye it is a simple memory game sha where the computer choses one random box for each level and the player has to choose the box that matches the one chosen by the computer.
+
+### For more information [CLICK HERE](https://en.wikipedia.org/wiki/Simon_(game))
+
+## [Link to the page]()
--- game.js
+++ game.js
@@ -2,74 +2,68 @@ const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
 const userClickedPattern = [];
 let level = 0;
-
+let started = false;
 // Starting the game
+
 $(document).keypress(function (e) {
-  $(this).unbind("keypress");
-  nextSequence();
-  $("h1").text(`Level ${level}`); // leveling up
+  if (!started) {
+    $("h1").text(`Level ${level}`); // leveling up
+    nextSequence();
+    started = true;
+  }
+  // $(this).unbind("keypress");
 });
+
+$(".btn").click(function () {
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  // Play sound and animate
+  playSound(userChosenColour);
+   animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1); //check answer after user clicked
+});
+
 
 // checking answers
 function checkAnswer(currentLevel) {
   currentLevel = currentLevel.toString();
 
-  if (currentLevel === gamePattern[gamePattern.length - 1]) { 
-    console.log(
-      "success",
-      gamePattern,
-      `Clicked pattern: ${userClickedPattern}`
-    );
- 
-  }
-  // if (gamePattern.indexOf(currentLevel) === gamePattern.length - 1) {
-  //   console.log(
-  //     `success`,
-  //     `Clicked pattern: ${userClickedPattern}`,
-  //     `gamePattern: ${gamePattern}`
-  //   );
-  // }
-  // nextSequence();
-  // if (nextSequence() === true) {
-  //   setInterval(function () {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
 
-  //   }, 1000);
-  // }
-  // else {
-  //   playSound("wrong");
-  //   // nextSequence();
-  // }
+      userClickedPattern.splice(0, userClickedPattern.length);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
 }
 
 function nextSequence() {
+  userClickedPattern.splice(0, userClickedPattern.length);
+  $(`h1`).text(`Level ${++level}`); //Incementing levels
   var randomNumber = Math.floor(Math.random() * 4); //0 - 3
   var randomChosenColour = buttonColours[randomNumber]; //random color from the array of buttonColours
   gamePattern.push(randomChosenColour); //  push the random colors generated into the gamePattern array
-
-  $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100); // Pick random divs with random colours
-  animatePress();
-
-  // animate and play sound
-  playSound(randomChosenColour).click(function () {
-    // animate current colors when clicked
-    $(this).fadeOut(100).fadeIn(100);
-    animatePress(randomChosenColour);
-    let userChosenColour = $(this).attr("id");
-    checkAnswer(userChosenColour); //check answer after user clicked
-    userClickedPattern.push(userChosenColour);
-
-    // Play sound when clicked
-    const audio = new Audio(`sounds/${userChosenColour}.mp3`);
-    audio.play();
-  });
-
-  $(`h1`).text(`Level ${++level}`); //Incementing levels
+  $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100); // Pick random divs with random colours
+  playSound(randomChosenColour);
 }
 
 // To animate keys
 function animatePress(currentColour) {
   $(`#${currentColour}`).addClass("pressed");
-  return setTimeout(() => {
+  setTimeout(() => {
     $(`#${currentColour}`).removeClass("pressed");
   }, 100);
 }
@@ -82,6 +76,12 @@ function playSound(name) {
   });
 }
 
+// notify when you need to start over
+function startOver() {
+  level = 0;
+ gamePattern.splice(0, gamePattern.length);
+  started = false;
+}
 // $("document").ready(function () {
 //   const audio = new Audio("source.mp3");
 //   audio.play();
